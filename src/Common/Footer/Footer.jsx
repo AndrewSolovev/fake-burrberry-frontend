096981089bbc9e691127a560436677f9ab9a90fc
Andrew <andrewsolovev@gmail.com>
2017-08-10 01:39:46 +0300

    Feat: Footer styles moved from css to styled-components

--- src/Common/Footer/Footer.jsx
+++ src/Common/Footer/Footer.jsx
@@ -1,47 +1,139 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Footer = styled.footer`
+  margin-top: 4rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  background-color: #f3f3f3;
+
+  @media only screen and (min-width: 768px) {
+    margin-top: 2rem;
+    padding-top: 0;
+    padding-bottom: 2rem;
+  }
+`
+
+const Nav = styled.nav`
+  display: none;
+  padding-bottom: 0.25rem;
+
+  @media only screen and (min-width: 768px) {
+    display: block;
+  }
+`
+
+const NavTitle = styled.h3`
+  margin: 0;
+  padding-top: 2rem;
+  padding-bottom: 1rem;
+  font-size: 0.75rem;
+`
+
+const NavLink = styled.a`
+  display: block;
+  padding-bottom: 0.75rem;
+  font-size: 0.75rem;
+  line-height: 1rem;
+  color: #999999;
+`
+const LocationButtons = styled.div`
+@media only screen and (min-width: 768px) {
+  display: flex;
+}
+`
+
+const LocationButton = styled.button`
+  display: block;
+  padding: 1rem 0.5rem 0rem 0.5rem;
+  border: none;
+  font-size: 0.75rem;
+  font-weight: 400;
+  font-family: 'Raleway', sans-serif;
+  line-height: 16px;
+  color: #999999;
+  background-color: #f3f3f3;
+
+  @media only screen and (min-width: 768px) {
+    margin-right: 1.5rem;
+    margin-left: 0rem;
+    padding-left: 0;
+    padding-right: 0;
+  }
+`
+
+const NeedHelp = styled.h2`
+  margin: 0;
+  padding-top: 1.5rem;
+  text-align: center;
+  font-size: 1rem;
+  line-height: 19px;
+  color: #171717;
+  font-weight: 700;
+
+  @media only screen and (min-width: 768px) {
+    display: none;
+  }
+`
+
+const Contact = styled.a`
+  margin: 0;
+  display: block;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  font-size: 0.875rem;
+  text-align: center;
+  font-family: 'Lora', serif;
+  font-style: italic;
+  line-height: 17px;
+
+  @media only screen and (min-width: 768px) {
+    display: none;
+  }
+`
 
 export default () => {
   return (
-    <footer>
-    <div className="container">
-      <nav className="footer-nav">
-        <div className="row">
-          <div className="col-md-3">
-            <h3 className="footer-nav-title">CUSTOMER SERVICE</h3>
-              <a className="footer-nav-link">Contact Us</a>
-              <a className="footer-nav-link">Payment</a>
-              <a className="footer-nav-link">Payment</a>
-              <a className="footer-nav-link">Returns</a>
-              <a className="footer-nav-link">Faqs</a>
-              <a className="footer-nav-link">Live Chat</a>
-              <a className="footer-nav-link">The Burberry App</a>
-              <a className="footer-nav-link">Store Locator</a>
+    <Footer>
+      <div className="container">
+        <Nav>
+          <div className="row">
+            <div className="col-md-3">
+              <NavTitle>CUSTOMER SERVICE</NavTitle>
+                <NavLink>Contact Us</NavLink>
+                <NavLink>Payment</NavLink>
+                <NavLink>Payment</NavLink>
+                <NavLink>Returns</NavLink>
+                <NavLink>Faqs</NavLink>
+                <NavLink>Live Chat</NavLink>
+                <NavLink>The Burberry App</NavLink>
+                <NavLink>Store Locator</NavLink>
+            </div>
+            <div className="col-md-3">
+              <NavTitle>OUR COMPANY</NavTitle>
+                <NavLink>Our History</NavLink>
+                <NavLink>Burberry Group Plc</NavLink>
+                <NavLink>Careers</NavLink>
+                <NavLink>Corporate Responsibility</NavLink>
+                <NavLink>Site Map</NavLink>
+            </div>
+            <div className="col-md-3">
+              <NavTitle>LEGAL & COOKIES</NavTitle>
+                <NavLink>Terms & Conditions</NavLink>
+                <NavLink>Privacy Policy</NavLink>
+                <NavLink>Cookie Policy</NavLink>
+                <NavLink>Accessibility Statement</NavLink>
+                <NavLink>Japan Only - SCTL indications</NavLink>
+            </div>
           </div>
-          <div className="col-md-3">
-            <h3 className="footer-nav-title">OUR COMPANY</h3>
-              <a className="footer-nav-link">Our History</a>
-              <a className="footer-nav-link">Burberry Group Plc</a>
-              <a className="footer-nav-link">Careers</a>
-              <a className="footer-nav-link">Corporate Responsibility</a>
-              <a className="footer-nav-link">Site Map</a>
-          </div>
-          <div className="col-md-3">
-            <h3 className="footer-nav-title">LEGAL & COOKIES</h3>
-              <a className="footer-nav-link">Terms & Conditions</a>
-              <a className="footer-nav-link">Privacy Policy</a>
-              <a className="footer-nav-link">Cookie Policy</a>
-              <a className="footer-nav-link">Accessibility Statement</a>
-              <a className="footer-nav-link">Japan Only - SCTL indications</a>
-          </div>
-        </div>
-      </nav>
-      <div className="footer-language-buttons">
-        <button className="footer-language-button" type="button">Shipping country: RussiaFederation</button>
-        <button className="footer-language-button" type="button">Language: English</button>
+        </Nav>
+        <LocationButtons>
+          <LocationButton type="button">Shipping country: RussiaFederation</LocationButton>
+          <LocationButton type="button">Language: English</LocationButton>
+        </LocationButtons>
+        <NeedHelp>Need help?</NeedHelp>
+        <Contact>Find out more and contact us</Contact>
       </div>
-      <h2 className="footer-needhelp">Need help?</h2>
-      <a className="footer-contact">Find out more and contact us</a>
-    </div>
-    </footer>
-);
+    </Footer>
+  );
 };

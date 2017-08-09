94664755e4b080a589f9bd285bfb43c6b39f0f0c
Andrew <andrewsolovev@gmail.com>
2017-08-10 00:58:53 +0300

    Feat: Shipping section divided to components

--- src/Product/Product.jsx
+++ src/Product/Product.jsx
@@ -8,6 +8,7 @@ import Hr from "../Common/Hr/Hr";
 import SectionHeading from "./SectionHeading/SectionHeading";
 import Textarea from "./Textarea/Textarea";
 import WeRecommend from "./WeRecommend/WeRecommend";
+import Shipping from "./Shipping/Shipping";
 
 
 
@@ -15,53 +16,39 @@ class Product extends Component {
   render() {
     return (
       <div>
-      <Helmet>
-        <title>Long Cotton Gabardine Car Coat | Men - Burberry</title>
-      </Helmet>
-      <section className="container">
-        <ProductTitle productName="Long Cotton Gabardine Car Coat"></ProductTitle>
-        <ProductCard></ProductCard>
-      </section>
+        <Helmet>
+          <title>Long Cotton Gabardine Car Coat | Men - Burberry</title>
+        </Helmet>
 
-      <Hr></Hr>
+        <section className="container">
+          <ProductTitle productName="Long Cotton Gabardine Car Coat"></ProductTitle>
+          <ProductCard></ProductCard>
+        </section>
 
-      <section className="container">
-        <SectionHeading sectionName="DESCRIPTION"></SectionHeading>
-        <Textarea></Textarea>
-      </section>
+        <Hr></Hr>
 
-      <Hr></Hr>
+        <section className="container">
+          <SectionHeading sectionName="DESCRIPTION"></SectionHeading>
+          <Textarea></Textarea>
+        </section>
 
-      <section className="container">
-        <SectionHeading mobileHidden sectionName="SHIPPING & RETURNS"></SectionHeading>
-        <div className="row">
-          <div className="col-md-7">
-            <img className="product-shipping-img"src="/shipping-img.jpg" alt="two burberry's boxes with tape"/>
-          </div>
-          <div className="col-md-5">
-            <h2 className="product-shipping-title">DELIVERY</h2>
-            <h3 className="product-shipping-options-title">Free Next Day Delivery</h3>
-              <p className="product-shipping-options-desc">Order before 7pm Monday to Thursday for delivery thenext day</p>
-            <h3 className="product-shipping-options-title">Collect-in-Store</h3>
-              <p className="product-shipping-options-desc">Order online today and pick up your items in storeas early as tomorrow</p>
-            <h3 className="product-shipping-options-title">Free Returns</h3>
-              <p className="product-shipping-options-desc">Enjoy free returns on your order</p>
-            <h3 className="product-shipping-options-title">Free Gift Packaging</h3>
-              <p className="product-shipping-options-desc">Discover our gift packaging, a gold lined box tied with a coloured ribbon</p>
-          </div>
-        </div>
-      </section>
+        <Hr></Hr>
 
-      <Hr></Hr>
+        <section className="container">
+          <SectionHeading mobileHidden sectionName="SHIPPING & RETURNS"></SectionHeading>
+          <Shipping></Shipping>
+        </section>
 
-      <WeRecommend></WeRecommend>
+        <Hr></Hr>
 
-      <section className="container">
-        <h2 className="product-moreforyou-title">MORE FOR YOU</h2>
-        <a className="product-moreforyou-link">Men's Black Trench Coats</a>
-        <a className="product-moreforyou-link">Men's Short Trench Coats</a>
-        <a className="product-moreforyou-link">Men's Long Trench Coats</a>
-      </section>
+        <WeRecommend></WeRecommend>
+
+        <section className="container">
+          <h2 className="product-moreforyou-title">MORE FOR YOU</h2>
+          <a className="product-moreforyou-link">Men's Black Trench Coats</a>
+          <a className="product-moreforyou-link">Men's Short Trench Coats</a>
+          <a className="product-moreforyou-link">Men's Long Trench Coats</a>
+        </section>
       </div>
     );
   }
--- src/Product/WeRecommend/WeRecommend.jsx
+++ src/Product/WeRecommend/WeRecommend.jsx
@@ -4,20 +4,45 @@ import styled from 'styled-components';
 import ProductCard from "./ProductCard/ProductCard";
 
 
+const Title = styled.h2`
+  display: inline-block;
+  width: auto;
+  margin: 0;
+  margin-left: 0.5rem;
+  padding-top: 3rem;
+  padding-bottom: 0.5rem;
+  border-bottom: 1px solid #171717;
+  font-family: 'Raleway', sans-serif;
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 19px;
+  @media only screen and (min-width: 768px) {
+    padding-top: 1.5rem;
+    margin-left: 0;
+  }
+`
+
+const Container = styled.div`
+  padding-top: 1.5rem;
+  @media only screen and (min-width: 768px) {
+    padding-top: 1rem;
+  }
+`
+
 
 class WeRecommend extends Component {
   render() {
     return (
       <section className="container">
-        <h2 className="product-recommendations-title">WE RECOMMEND</h2>
-        <div className="product-recommendations-goods">
+        <Title>WE RECOMMEND</Title>
+        <Container>
           <div className="row">
             <ProductCard src="/rec_1.jpg" alt="man in hoodie"></ProductCard>
             <ProductCard src="/rec_2.jpg" alt="man in jeans"></ProductCard>
             <ProductCard src="/rec_3.jpg" alt="bag"></ProductCard>
             <ProductCard src="/rec_4.jpg" alt="shoes"></ProductCard>
           </div>
-        </div>
+        </Container>
       </section>
     );
   };

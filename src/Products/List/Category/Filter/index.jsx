9934c74d61e890cc9af8a6528d4db71035d8c6f2
Andrew <andrewsolovev@gmail.com>
2017-08-19 17:18:03 +0300

    Fix: Deleted refine by

--- src/Products/List/Category/Filter/index.jsx
+++ src/Products/List/Category/Filter/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import MediaQuery from 'react-responsive';
-import breakpoints from '../../../../breakpoints';
 import Option from './Option';
 
 const Filter = styled.div`
@@ -17,21 +15,8 @@ const Filter = styled.div`
   }
 `;
 
-const Refine = styled.p`
-  margin: 0;
-  margin-top: 2rem;
-  margin-right: 2rem;
-  font-size: 0.75rem;
-  line-height: 1rem;
-  color: #171717;
-  font-weight: 400;
-  flex-shrink: 0;
-`;
 export default () =>
   (<Filter>
-    <MediaQuery maxWidth={breakpoints.sm - 1}>
-      <Refine>Refine by</Refine>
-    </MediaQuery>
     <Option value="Category" />
     <Option value="Colour" />
     <Option value="Size" />
